f07fc69c5cd812ae6e7bdefaa475023d34fefbe7
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2021-02-27 09:23:59 +0100

    Added workbox with update behavior

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import wb from "./registerServiceWorker"
 
+Vue.prototype.$workbox = wb
 Vue.config.productionTip = false
 
 new Vue({
