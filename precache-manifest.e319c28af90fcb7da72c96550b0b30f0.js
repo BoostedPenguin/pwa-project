662f5371186c501633f3e0c36ca4723a51ff4ae7
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2021-03-19 16:20:47 +0100

    deploy

---
+++ precache-manifest.e319c28af90fcb7da72c96550b0b30f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af32444c2cb130a13e7",
+    "url": "/pwa-project/css/app.56ca29c9.css"
+  },
+  {
+    "revision": "5b6e7bd39758158c5dfb",
+    "url": "/pwa-project/css/chunk-vendors.ab10bd22.css"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/pwa-project/img/main-logo.7ea4af7d.png"
+  },
+  {
+    "revision": "ee50f2ec48cf5f8b08c157f1af663e31",
+    "url": "/pwa-project/index.html"
+  },
+  {
+    "revision": "7af32444c2cb130a13e7",
+    "url": "/pwa-project/js/app.0cf008c8.js"
+  },
+  {
+    "revision": "5b6e7bd39758158c5dfb",
+    "url": "/pwa-project/js/chunk-vendors.f58a2724.js"
+  },
+  {
+    "revision": "05f8c3af1bd1d2a4df9d80500dda6522",
+    "url": "/pwa-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-project/robots.txt"
+  }
+]);
